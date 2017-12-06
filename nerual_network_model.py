344d74c3adb80b5151830105d40e2e8237790231
daidongyang <daidongyang12@gmail.com>
2017-12-06 16:28:07 +0800

    cuda visible devicesfe

--- nerual_network_model.py
+++ nerual_network_model.py
@@ -5,6 +5,9 @@ import tempfile
 import sys
 import config
 import post_process
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = config.visible_device
 
 
 def deepnn(x, n_class):
@@ -14,44 +17,44 @@ def deepnn(x, n_class):
            n_class: the number of classes
     :return: A tuple (y, keep_prob). y is a tensor of shape (N_example, len(classes)).
     """
-    with tf.device(config.device):
-        with tf.name_scope('reshape'):
-            x_mfcc = tf.reshape(x, [-1, 39, 1, 1])
 
-        with tf.name_scope('conv1'):
-            w_conv1 = weight_variable([5, 1, 1, 32])
-            b_conv1 = bias_variable([32])
-            h_conv1 = tf.nn.relu(conv2d(x_mfcc, w_conv1) + b_conv1)
+    with tf.name_scope('reshape'):
+        x_mfcc = tf.reshape(x, [-1, 39, 1, 1])
 
-        with tf.name_scope('pool1'):
-            # n_sample * 20 * 1 * 32
-            h_pool1 = max_pool_2x1(h_conv1)
+    with tf.name_scope('conv1'):
+        w_conv1 = weight_variable([5, 1, 1, 32])
+        b_conv1 = bias_variable([32])
+        h_conv1 = tf.nn.relu(conv2d(x_mfcc, w_conv1) + b_conv1)
 
-        with tf.name_scope('conv2'):
-            w_conv2 = weight_variable([5, 1, 32, 64])
-            b_conv2 = bias_variable([64])
-            h_conv2 = tf.nn.relu(conv2d(h_pool1, w_conv2) + b_conv2)
+    with tf.name_scope('pool1'):
+        # n_sample * 20 * 1 * 32
+        h_pool1 = max_pool_2x1(h_conv1)
 
-        with tf.name_scope('pool2'):
-            # n_sample * 10 * 1 * 64
-            h_pool2 = max_pool_2x1(h_conv2)
+    with tf.name_scope('conv2'):
+        w_conv2 = weight_variable([5, 1, 32, 64])
+        b_conv2 = bias_variable([64])
+        h_conv2 = tf.nn.relu(conv2d(h_pool1, w_conv2) + b_conv2)
 
-        with tf.name_scope('fc1'):
-            w_fc1 = weight_variable([10 * 1 * 64, 256])
-            b_fc1 = weight_variable([256])
-            h_pool2_flat = tf.reshape(h_pool2, [-1, 10 * 1 * 64])
-            h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, w_fc1) + b_fc1)
+    with tf.name_scope('pool2'):
+        # n_sample * 10 * 1 * 64
+        h_pool2 = max_pool_2x1(h_conv2)
 
-        with tf.name_scope('dropout'):
-            keep_prob = tf.placeholder(tf.float32)
-            h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+    with tf.name_scope('fc1'):
+        w_fc1 = weight_variable([10 * 1 * 64, 256])
+        b_fc1 = weight_variable([256])
+        h_pool2_flat = tf.reshape(h_pool2, [-1, 10 * 1 * 64])
+        h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, w_fc1) + b_fc1)
 
-        with tf.name_scope('fc2'):
-            w_fc2 = weight_variable([256, n_class])
-            b_fc2 = bias_variable([n_class])
-            y_conv = tf.matmul(h_fc1_drop, w_fc2) + b_fc2
+    with tf.name_scope('dropout'):
+        keep_prob = tf.placeholder(tf.float32)
+        h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-        return y_conv, keep_prob
+    with tf.name_scope('fc2'):
+        w_fc2 = weight_variable([256, n_class])
+        b_fc2 = bias_variable([n_class])
+        y_conv = tf.matmul(h_fc1_drop, w_fc2) + b_fc2
+
+    return y_conv, keep_prob
 
 
 def conv2d(x, W):
@@ -89,22 +92,21 @@ def main(_):
 
     y_conv, keep_prob = deepnn(x, len(classes))
 
-    with tf.device(config.device):
-        with tf.name_scope('loss'):
-            cross_entroy = tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv)
-        cross_entroy = tf.reduce_mean(cross_entroy)
+    with tf.name_scope('loss'):
+        cross_entroy = tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv)
+    cross_entroy = tf.reduce_mean(cross_entroy)
 
-        with tf.name_scope('adam_optimizer'):
-            train_step = tf.train.AdadeltaOptimizer(config.learning_rate).minimize(cross_entroy)
+    with tf.name_scope('adam_optimizer'):
+        train_step = tf.train.AdadeltaOptimizer(config.learning_rate).minimize(cross_entroy)
 
-        with tf.name_scope('accuracy'):
-            correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
-            correct_prediction = tf.cast(correct_prediction, tf.float32)
-        accuracy = tf.reduce_mean(correct_prediction)
+    with tf.name_scope('accuracy'):
+        correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+        correct_prediction = tf.cast(correct_prediction, tf.float32)
+    accuracy = tf.reduce_mean(correct_prediction)
 
-        with tf.name_scope('result'):
-            g_truth = tf.argmax(y_, 1)
-            p_rs = tf.argmax(y_conv, 1)
+    with tf.name_scope('result'):
+        g_truth = tf.argmax(y_, 1)
+        p_rs = tf.argmax(y_conv, 1)
 
     graph_location = tempfile.mkdtemp()
     print('Saving graph to: %s' % graph_location)
@@ -152,7 +154,6 @@ def main(_):
         post_process.dump_list(classes, config.classes_pickle)
         post_process.dump_list(ground_truth, config.gt_pickle)
         post_process.dump_list(predict_results, config.pr_pickle)
-
 
 if __name__ == '__main__':
     tf.app.run(main=main, argv=[sys.argv[0]])
